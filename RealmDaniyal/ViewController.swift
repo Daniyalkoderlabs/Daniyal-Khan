cc9e93613c7b0410ed36107d36460c61ada29a04
Daniyal Yousuf <daniyal.yousuf@koderlabs.com>
2018-09-26 14:58:07 +0500

    Initial Commit

---
+++ RealmDaniyal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmDaniyal
+//
+//  Created by Daniyal Yousuf on 9/26/18.
+//  Copyright © 2018 Daniyal Yousuf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
